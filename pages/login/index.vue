fa8b6ebf412f7ae8eb0688fab1fdb52e7dcade35
Farhan <ishraaq21@gmail.com>
2022-01-22 20:29:05 +0600

    wip: components & pages

---
+++ pages/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Login
+
+    <button class="button button--primary">
+      Login
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LoginPage',
+})
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Users
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'UsersPage',
+
+  fetch() {
+    return this.$axios.get('/api/users')
+      .then(response => response.data)
+  },
+})
+</script>
